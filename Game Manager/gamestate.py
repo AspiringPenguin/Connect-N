a53964a1ea33a89925816f3cf74278a7117956ba
AspiringPenguin <253924359+AspiringPenguin@users.noreply.github.com>
2026-06-16 19:28:12 +0100

    Filled out board class

--- Game Manager/gamestate.py
+++ Game Manager/gamestate.py
@@ -1,5 +1,3 @@
-import typing
-
 class Board:
     def __init__(self, height : int=6, width : int=7, aim : int=4):
         #Store setup
@@ -8,24 +6,37 @@ class Board:
         self.aim = aim
 
         #Setup board array
-        board = list[list[int]]() #0 = empty. 1=red, -1=yellow
+        self.board = list[list[int]]() #0=empty,  1=red, -1=yellow
         for y in range(height):
-            board.append(list[int]())
+            self.board.append(list[int]())
             for x in range(width):
-                board[y].append(0)
+                self.board[y].append(0)
 
         #Helper variables
         self.full = [False] * width
-        self.height = [0] * width
+        self.heights = [0] * width
 
         #Game state
         self.toMove = 1 # Red
 
-    def makeMove(self, move : typing.Any):
-        pass
+    def makeMove(self, move : int): #Integer of the column to play in
+        self.board[self.heights[move]][move] = self.toMove
+        self.heights[move] += 1
+        if self.heights[move] == self.height:
+            self.full[move] = True
 
-    def undoMove(self):
-        pass
+        #Get ready for the next move
+        self.toMove *= -1
+
+    def undoMove(self, move : int):
+        self.toMove *= -1
+        
+        self.board[self.heights[move]][move] = 0
+        self.heights[move] -= 1
+        self.full[move] = False
+
+    def getMoves(self) -> list[int]:
+        return [column for column in range(self.width) if not self.full[column]]
 
     def getMoves(self):
         pass
